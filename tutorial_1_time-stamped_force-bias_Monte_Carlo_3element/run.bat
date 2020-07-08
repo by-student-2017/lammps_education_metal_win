2f4937517593fcf78d5c093b189eab777ea64121
by-student-2017 <studentsctest@gmail.com>
2020-07-08 14:17:55 +0900

    windows10

---
+++ tutorial_1_time-stamped_force-bias_Monte_Carlo_3element/run.bat
@@ -0,0 +1,5 @@
+mkdir cfg
+"C:\Program Files\LAMMPS 64-bit 18Jun2019\bin\lmp_serial.exe" -in in.lmp
+
+REM AgTaO3 case (use in.AgTaO3 and AgTaO3_40atoms.dat)
+REM "C:\Program Files\LAMMPS 64-bit 18Jun2019\bin\lmp_serial.exe" -in in.AgTaO3
