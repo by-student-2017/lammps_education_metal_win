fe5f5657ccc3ab3d168ca876e77f9048bb646a20
by-student-2017 <studentsctest@gmail.com>
2020-07-13 20:22:52 +0900

    windows10

--- Modified_Phonon-FCC-Cu-EAM/run.bat
+++ Modified_Phonon-FCC-Cu-EAM/run.bat
@@ -14,25 +14,10 @@ cd ..
 "C:\Program Files\LAMMPS 64-bit 18Jun2019\bin\lmp_serial.exe" -screen none -in in.lmp
 
 type .\plot_settings\in.disp | .\phana_cygwin\phana.exe -in .\data\Phonon.bin.650000
-findstr /V "post" < pdisp.gnuplot > tmp
-findstr /V "pdisp.eps" < tmp > pdisp.gnuplot
-echo set terminal pngcairo color enhanced font ",10" > plot.tmp
-echo set out 'disp.png' >> plot.tmp
-copy plot.tmp+pdisp.gnuplot plot.disp
-gnuplot plot.disp
+gnuplot .\plot_settings\plot.disp
 
 type .\plot_settings\in.dos | .\phana_cygwin\phana.exe -in .\data\Phonon.bin.650000
-findstr /V "post" < pdos.gnuplot > tmp
-findstr /V "pdos.eps" < tmp > pdos.gnuplot
-echo set terminal pngcairo color enhanced font ",10" > plot.tmp
-echo set out 'dos.png' >> plot.tmp
-copy plot.tmp+pdos.gnuplot plot.dos
-gnuplot plot.dos
+gnuplot .\plot_settings\plot.dos
 
-move plot.disp .\plot_settings
-move  pdos.gnuplot .\plot_settings
-
-del plot.tmp
-del disp.gnuplot
-del pdos.gnuplot
-del tmp
+del pdisp.gnuplot
+del  pdos.gnuplot
