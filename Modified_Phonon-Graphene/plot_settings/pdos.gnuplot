5962bcf8ba180a78f4ff4f42771f03f4dfb7ff93
by-student-2017 <studentsctest@gmail.com>
2021-04-11 21:12:27 +0900

    windows 10

--- Modified_Phonon-Graphene/plot_settings/pdos.gnuplot
+++
@@ -1,5 +0,0 @@
-
-set xlabel "frequency (THz)"
-set ylabel "Phonon DOS"
-unset key
-plot "dos.dat" u 1:2 w l
--- Modified_Phonon-Graphene/run_plot.bat
+++
@@ -1,8 +0,0 @@
-type .\plot_settings\in.disp | .\phana_cygwin\phana.exe -in .\data\Phonon.bin.1200000
-gnuplot .\plot_settings\plot.disp
-
-type .\plot_settings\in.dos | .\phana_cygwin\phana.exe -in .\data\Phonon.bin.1200000
-gnuplot .\plot_settings\plot.dos
-
-del pdisp.gnuplot
-del  pdos.gnuplot
