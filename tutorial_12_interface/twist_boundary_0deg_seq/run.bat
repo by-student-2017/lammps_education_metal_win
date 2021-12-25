d2c5b6422cea494ef1dfe44c5622192e4acc7835
by-student-2017 <studentsctest@gmail.com>
2021-12-25 23:35:43 +0900

    windows10

---
+++ tutorial_12_interface/twist_boundary_0deg_seq/run.bat
@@ -0,0 +1,14 @@
+
+for %%i in (0.0 0.2 0.4 0.6 0.8 1.0 1.2 1.4 1.6 1.8 2.0 2.2 2.4 2.6 2.8 3.0 3.2 3.4 3.6 3.8 4.0) do (
+
+  echo clear >> tmp.lmp
+  echo variable x_displace equal %%i >> tmp.lmp
+  
+  copy /b tmp.lmp + in.p1.lmp in.lmp
+
+  "C:\Program Files\LAMMPS 64-bit 18Jun2019\bin\lmp_serial.exe" -in in.lmp
+
+  del tmp.lmp
+)
+
+plot.gnu.gpl
