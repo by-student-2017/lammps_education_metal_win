cbe4b6d7a58f9b0c22bc00cb473c1ccfc66051ba
by-student-2017 <studentsctest@gmail.com>
2021-12-26 14:35:48 +0900

    windows10

--- tutorial_12_interface/twist_boundary_0deg_seq/run.bat
+++ tutorial_12_interface/twist_boundary_0deg_seq/run.bat
@@ -1,9 +1,10 @@
 mkdir cfg
 
-for %%i in (0.0 0.2 0.4 0.6 0.8 1.0 1.2 1.4 1.6 1.8 2.0 2.2 2.4 2.6 2.8 3.0 3.2 3.4 3.6 3.8 4.0) do (
+REM %%i/10 = 0.01 [nm] step
+for /l %%i in (0, 1, 40) do (
 
   echo clear >> tmp.lmp
-  echo variable x_displace equal %%i >> tmp.lmp
+  echo variable x_displace equal %%i/10 >> tmp.lmp
   
   copy /b tmp.lmp + in.p1.lmp in.lmp
 
