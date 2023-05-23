ea65873966903751d86462af7c7df5743f080cc6
by-student-2017 <studentsctest@gmail.com>
2023-05-23 16:49:06 +0900

    windows11

--- Reference_Phonon-FCC-Cu-EAM_MSMPI/run_plot.bat
+++ Reference_Phonon-FCC-Cu-EAM_MSMPI/run_plot.bat
@@ -1,8 +1,8 @@
 type .\plot_settings\in.disp | .\phana_cygwin\phana.exe -in .\data\Phonon.bin.650000
-gnuplot .\plot_settings\plot.disp
+"C:\Program Files\gnuplot\bin\gnuplot.exe" .\plot_settings\plot.disp
 
 type .\plot_settings\in.dos | .\phana_cygwin\phana.exe -in .\data\Phonon.bin.650000
-gnuplot .\plot_settings\plot.dos
+"C:\Program Files\gnuplot\bin\gnuplot.exe" .\plot_settings\plot.dos
 
 del pdisp.gnuplot
 del  pdos.gnuplot
