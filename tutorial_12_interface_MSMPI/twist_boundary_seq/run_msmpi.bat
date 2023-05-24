ed93a621f765ac0d9f32f1ba0ec18beacf7574da
by-student-2017 <studentsctest@gmail.com>
2023-05-24 15:43:19 +0900

    windows11

---
+++ tutorial_12_interface_MSMPI/twist_boundary_seq/run_msmpi.bat
@@ -0,0 +1,23 @@
+set ncore=4
+
+mkdir cfg
+
+REM %%i/20 = 0.005 [nm] * 2 step
+for /l %%i in (-10,2,80) do (
+
+  echo clear >> tmp.lmp
+  echo variable x_displace equal %%i/20 >> tmp.lmp
+  
+  copy /b tmp.lmp + in.p1.lmp in.lmp
+
+  "C:\Program Files\Microsoft MPI\Bin\mpiexec.exe" -np %ncore% "C:\Program Files\LAMMPS 64-bit 22Dec2022-MSMPI\bin\lmp.exe" -in in.lmp
+  
+  cd cfg
+  move run.init.cfg   run.init.%%i.cfg
+  move run.finish.cfg run.finish.%%i.cfg
+  cd ..
+
+  del tmp.lmp
+)
+
+plot.gnu.gpl
