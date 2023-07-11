d91f9711508e08e6be535905b88ceb1ec0fcd70c
by-student-2017 <studentsctest@gmail.com>
2023-07-11 16:29:17 +0900

    windows11

---
+++ references/Fe-Ni-Cr_alloys/run_msmpi.bat
@@ -0,0 +1,8 @@
+set ncore=4
+
+for %%1 in (calc_fcc_ver1.in) do (
+  "C:\Program Files\Microsoft MPI\Bin\mpiexec.exe" -np %ncore% "C:\Program Files\LAMMPS 64-bit 22Dec2022-MSMPI\bin\lmp.exe" -in %%1
+  echo input file: %%1
+)
+
+pause
