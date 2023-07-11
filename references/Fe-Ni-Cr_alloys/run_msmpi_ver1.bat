0d7af42d90bb55b2962a788aed5336f5b23f040c
by-student-2017 <studentsctest@gmail.com>
2023-07-11 16:34:24 +0900

    windows11

--- references/Fe-Ni-Cr_alloys/run_msmpi_ver1.bat
+++ references/Fe-Ni-Cr_alloys/run_msmpi_ver1.bat
@@ -1,4 +1,4 @@
-set ncore=4
+set ncore=1
 
 for %%1 in (calc_fcc_ver1.in) do (
   "C:\Program Files\Microsoft MPI\Bin\mpiexec.exe" -np %ncore% "C:\Program Files\LAMMPS 64-bit 22Dec2022-MSMPI\bin\lmp.exe" -in %%1
---
+++ references/Fe-Ni-Cr_alloys/run_msmpi_ver2.bat
@@ -0,0 +1,8 @@
+set ncore=1
+
+for %%1 in (calc_fcc_ver2.in) do (
+  "C:\Program Files\Microsoft MPI\Bin\mpiexec.exe" -np %ncore% "C:\Program Files\LAMMPS 64-bit 22Dec2022-MSMPI\bin\lmp.exe" -var latconst 4 -in %%1
+  echo input file: %%1
+)
+
+pause
