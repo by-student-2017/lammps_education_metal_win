45d39a1fbe07ea55d08c5346fa54d33b39427c59
by-student-2017 <studentsctest@gmail.com>
2023-08-04 15:45:41 +0900

    windows11

--- references/PdAgH/run_msmpi_PdAgH-0292-NPT.bat
+++ references/PdAgH/run_msmpi_PdAgH-0292-NPT.bat
@@ -1,6 +1,7 @@
 set ncore=4
 
 mkdir cfg
+mkdir xlength_vs_cohesive_energy
 
 for %%1 in (PdAgH-0292-NPT.in) do (
   "C:\Program Files\Microsoft MPI\Bin\mpiexec.exe" -np %ncore% "C:\Program Files\LAMMPS 64-bit 22Dec2022-MSMPI\bin\lmp.exe" -in %%1
