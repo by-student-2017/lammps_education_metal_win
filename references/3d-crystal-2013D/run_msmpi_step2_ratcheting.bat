0b31db50ed773d42ba34d5d63e477944e9c08bac
by-student-2017 <studentsctest@gmail.com>
2023-08-16 10:48:41 +0900

    windows11

--- references/3d-crystal-2013D/run_msmpi_step2_ratcheting.bat
+++ references/3d-crystal-2013D/run_msmpi_step2_ratcheting.bat
@@ -2,11 +2,9 @@ set ncore=4
 
 rem mkdir cfg
 
-for %%1 in (in_v2.lmp) do (
+for %%1 in (in_step2_ratcheting.lmp) do (
   "C:\Program Files\Microsoft MPI\Bin\mpiexec.exe" -np %ncore% "C:\Program Files\LAMMPS 64-bit 22Dec2022-MSMPI\bin\lmp.exe" -in %%1
   echo input file: %%1
 )
-
-plot_stress_vs_strain.gpl
 
 pause
