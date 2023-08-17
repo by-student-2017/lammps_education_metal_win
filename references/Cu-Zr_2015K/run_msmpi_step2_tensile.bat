fd2305413e249018caef002358944f7edf72b64e
by-student-2017 <studentsctest@gmail.com>
2023-08-17 14:59:14 +0900

    windows11

--- references/Cu-Zr_2015K/run_msmpi_step2_tensile.bat
+++ references/Cu-Zr_2015K/run_msmpi_step2_tensile.bat
@@ -3,7 +3,7 @@ set ncore=4
 mkdir cfg
 mkdir stress
 
-for %%1 in (in_A3.lmp) do (
+for %%1 in (in_step2_tensile.lmp) do (
   "C:\Program Files\Microsoft MPI\Bin\mpiexec.exe" -np %ncore% "C:\Program Files\LAMMPS 64-bit 22Dec2022-MSMPI\bin\lmp.exe" -in %%1
   echo input file: %%1
 )
