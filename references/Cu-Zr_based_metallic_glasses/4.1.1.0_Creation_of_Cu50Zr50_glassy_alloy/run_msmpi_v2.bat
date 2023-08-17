d7197e54018dc1ff8d32f1238cf4731535b6f03d
by-student-2017 <studentsctest@gmail.com>
2023-08-17 16:59:18 +0900

    windows11

--- references/Cu-Zr_based_metallic_glasses/4.1.1.0_Creation_of_Cu50Zr50_glassy_alloy/run_msmpi_v2.bat
+++ references/Cu-Zr_based_metallic_glasses/4.1.1.0_Creation_of_Cu50Zr50_glassy_alloy/run_msmpi_v2.bat
@@ -2,9 +2,11 @@ set ncore=4
 
 mkdir cfg
 
-for %%1 in (in.lmp) do (
+for %%1 in (in_v2.lmp) do (
   "C:\Program Files\Microsoft MPI\Bin\mpiexec.exe" -np %ncore% "C:\Program Files\LAMMPS 64-bit 22Dec2022-MSMPI\bin\lmp.exe" -in %%1
   echo input file: %%1
 )
+
+call plot_rdf.bat
 
 pause
