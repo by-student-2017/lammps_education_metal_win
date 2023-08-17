daa8d1382d99f78ccbb9d287d06217f094f9de57
by-student-2017 <studentsctest@gmail.com>
2023-08-17 13:26:57 +0900

    windows11

---
+++ references/Cu-Zr_2015K/plot_rdf.bat
@@ -0,0 +1,17 @@
+set file_name=Cu_Zr_quench_large_restart.rdf
+set ndata=100
+
+del new_rdf.dat
+
+for /f "tokens=1-4" %%a in (%file_name%) do (
+  if "%%b"=="%ndata%" (
+    echo; >> new_rdf.dat
+  ) else (
+    echo %%a %%b %%c %%d >> new_rdf.dat
+  )
+)
+
+plot_rdf_all_v2.gpl
+
+rem findstr /V/R/C "^* 100" %file_name% > new_rdf.dat
+rem findstr /V/R/C " 100$" %file_name% > new_rdf.dat
