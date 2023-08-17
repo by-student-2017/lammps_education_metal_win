53cad6017afc468fb0fb4391b9d4e6609aa12fa7
by-student-2017 <studentsctest@gmail.com>
2023-08-17 14:44:15 +0900

    windows11

--- references/Cu-Zr_2012S/run_msmpi_step1.bat
+++ references/Cu-Zr_2012S/run_msmpi_step1.bat
@@ -2,7 +2,7 @@ set ncore=4
 
 mkdir cfg
 
-for %%1 in (in_A2.lmp) do (
+for %%1 in (in_step1.lmp) do (
   "C:\Program Files\Microsoft MPI\Bin\mpiexec.exe" -np %ncore% "C:\Program Files\LAMMPS 64-bit 22Dec2022-MSMPI\bin\lmp.exe" -in %%1
   echo input file: %%1
 )
