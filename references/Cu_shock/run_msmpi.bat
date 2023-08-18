4ffb2340212b04a77ee7eca0cbb121fa0bda34bd
by-student-2017 <studentsctest@gmail.com>
2023-08-18 18:41:51 +0900

    windows11

--- references/Cu_shock/run_msmpi.bat
+++ references/Cu_shock/run_msmpi.bat
@@ -5,7 +5,7 @@ set temp=300         REM The desired initial temperature.
 
 mkdir cfg
 
-for %%1 in (in.Cu_shock) do (
+for %%1 in (in.lmp) do (
   "C:\Program Files\Microsoft MPI\Bin\mpiexec.exe" -np %ncore% "C:\Program Files\LAMMPS 64-bit 22Dec2022-MSMPI\bin\lmp.exe" -in %%1 -var pistvel %pistvel% -var system_size 10 -var temp_equil %temp% -var press_equil 0.0
   echo input file: %%1
 )
