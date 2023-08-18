54433723384ec042f733ec1f36ee4abfe7a239dd
by-student-2017 <studentsctest@gmail.com>
2023-08-18 18:13:48 +0900

    windows11

---
+++ references/Cu_defect/5.3.2_vertical_defect/plot_rdf.bat
@@ -0,0 +1,21 @@
+@echo off
+
+echo "Set up new_rdf.dat for plot_rdf_all_v2.gpl"
+
+set file_name=Cu_defect_vertical_Tensile.rdf
+set ndata=100
+
+del new_rdf.dat
+
+for /f "tokens=1-4" %%a in (%file_name%) do (
+  if "%%b"=="%ndata%" (
+    echo; >> new_rdf.dat
+  ) else (
+    echo %%a %%b %%c %%d >> new_rdf.dat
+  )
+)
+
+plot_rdf_all_v2.gpl
+
+rem findstr /V/R/C "^* 100" %file_name% > new_rdf.dat
+rem findstr /V/R/C " 100$" %file_name% > new_rdf.dat
