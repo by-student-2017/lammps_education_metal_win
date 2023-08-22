41adf8342798e868e4d299179f30b48242a73699
by-student-2017 <studentsctest@gmail.com>
2023-08-22 12:52:21 +0900

    windows11

--- references/Fe-Cr_MSD/A1_Fe/run_msmpi_step1.bat
+++ references/Fe-Cr_MSD/A1_Fe/run_msmpi_step1.bat
@@ -1,7 +1,7 @@
 set ncore=4
 
 mkdir cfg
-mkdir stress
+rem mkdir stress
 
 for %%1 in (in_step1.lmp) do (
   "C:\Program Files\Microsoft MPI\Bin\mpiexec.exe" -np %ncore% "C:\Program Files\LAMMPS 64-bit 22Dec2022-MSMPI\bin\lmp.exe" -in %%1
