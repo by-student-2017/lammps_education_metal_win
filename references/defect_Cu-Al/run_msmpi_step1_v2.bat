fe9749a7573255c26f6be804390f3e1194a3a047
by-student-2017 <studentsctest@gmail.com>
2023-08-23 10:07:58 +0900

    windows10

--- references/defect_Cu-Al/run_msmpi_step1_v2.bat
+++ references/defect_Cu-Al/run_msmpi_step1_v2.bat
@@ -1,8 +1,8 @@
 set ncore=4
 
-rem mkdir cfg
+mkdir cfg
 
-for %%1 in (in.*) do (
+for %%1 in (in_step1_v2.lmp) do (
   "C:\Program Files\Microsoft MPI\Bin\mpiexec.exe" -np %ncore% "C:\Program Files\LAMMPS 64-bit 22Dec2022-MSMPI\bin\lmp.exe" -in %%1
   echo input file: %%1
 )
