075a9ee5c39b9aec9c27f9d2e1c78b1b221edb62
by-student-2017 <studentsctest@gmail.com>
2023-08-24 17:37:43 +0900

    windows10

--- references/YBCO/run_msmpi_step2.bat
+++ references/YBCO/run_msmpi_step2.bat
@@ -2,8 +2,8 @@ set ncore=4
 
 mkdir frames_file
 
-for %%1 in (in_step2_Tension.lmp) do (
-  "C:\Program Files\Microsoft MPI\Bin\mpiexec.exe" -np %ncore% "C:\Program Files\LAMMPS 64-bit 22Dec2022-MSMPI\bin\lmp.exe" -in %%1 -var pistvel %pistvel% -var system_size 10 -var temp_equil %temp% -var press_equil 0.0
+for %%1 in (in_step2_PKA.lmp) do (
+  "C:\Program Files\Microsoft MPI\Bin\mpiexec.exe" -np %ncore% "C:\Program Files\LAMMPS 64-bit 22Dec2022-MSMPI\bin\lmp.exe" -in %%1
   echo input file: %%1
 )
 
