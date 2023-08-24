555c20fbcd265918429c6014b699af283cf0cb35
by-student-2017 <studentsctest@gmail.com>
2023-08-24 15:22:30 +0900

    windows10

--- references/PdH/run_msmpi_PdH0885.bat
+++ references/PdH/run_msmpi_PdH0885.bat
@@ -1,7 +1,6 @@
 set ncore=4
 
 mkdir cfg
-mkdir Press_vs_Volume
 
 for %%1 in (in_PdH0885.lmp) do (
   "C:\Program Files\Microsoft MPI\Bin\mpiexec.exe" -np %ncore% "C:\Program Files\LAMMPS 64-bit 22Dec2022-MSMPI\bin\lmp.exe" -in %%1
