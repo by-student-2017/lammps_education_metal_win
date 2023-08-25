07d91f138bcfe4aea92f874a2fac3368dff25374
by-student-2017 <studentsctest@gmail.com>
2023-08-25 16:20:26 +0900

    windows11

---
+++ references/ZrH2/Zr-H/run_msmpi_A7_v2.bat
@@ -0,0 +1,12 @@
+set ncore=4
+
+mkdir cfg
+
+for %%1 in (in_A7_v2.lmp) do (
+  "C:\Program Files\Microsoft MPI\Bin\mpiexec.exe" -np %ncore% "C:\Program Files\LAMMPS 64-bit 22Dec2022-MSMPI\bin\lmp.exe" -in %%1
+  echo input file: %%1
+)
+
+plot_stress_vs_strain_v2.gpl
+
+pause
