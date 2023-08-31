13f9ce058d9cdc005efbaa65a7d9ef47f2608ec3
by-student-2017 <studentsctest@gmail.com>
2023-08-31 15:02:20 +0900

    windows11

---
+++ references/Cu-Zr_based_metallic_glasses/4.2.1.4_Tensile_deformation_Ni-Zr-Nb-Al_glassy_alloys/sequential_test/plot_rdf.bat
@@ -0,0 +1,21 @@
+@echo off
+
+set file_name=CuZrAlNi_300k_Tensile.rdf
+set ndata=100
+
+echo "Set up new_rdf.dat for plot_rdf_all_v2.gpl"
+
+del new_rdf.dat
+
+for /f "tokens=1-4" %%a in (%file_name%) do (
+  if "%%b"=="%ndata%" (
+    echo; >> new_rdf.dat
+  ) else (
+    echo %%a %%b %%c %%d >> new_rdf.dat
+  )
+)
+
+plot_rdf_all_v2.gpl
+
+rem findstr /V/R/C "^* 100" %file_name% > new_rdf.dat
+rem findstr /V/R/C " 100$" %file_name% > new_rdf.dat
