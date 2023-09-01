ffe98fc3eb2fef3d94648f40840142cf3ab34aa4
by-student-2017 <studentsctest@gmail.com>
2023-09-01 10:12:26 +0900

    windows11

--- references/Cu-Zr_based_metallic_glasses/4.2.1.4_Tensile_deformation_Ni-Zr-Nb-Al_glassy_alloys/sequential_test/run_seq.bat
+++ references/Cu-Zr_based_metallic_glasses/4.2.1.4_Tensile_deformation_Ni-Zr-Nb-Al_glassy_alloys/sequential_test/run_seq.bat
@@ -2,28 +2,43 @@ echo "-------------------------------------------------------------"
 echo erate1e-1 %date% %time%
 cd ./erate1e-1
 call ../run_msmpi_v2.bat
+plot_stress_vs_strain_v2.gpl
+rem call ./../plot_rdf.bat
+rem plot_rdf_all_v2.gpl
 cd ../
 
 echo "-------------------------------------------------------------"
 echo erate1e-2 %date% %time%
 cd ./erate1e-2
 call ../run_msmpi_v2.bat
+plot_stress_vs_strain_v2.gpl
+rem call ./../plot_rdf.bat
+rem plot_rdf_all_v2.gpl
 cd ../
 
 echo "-------------------------------------------------------------"
 echo erate1e-3 %date% %time%
 cd ./erate1e-3
 call ../run_msmpi_v2.bat
+plot_stress_vs_strain_v2.gpl
+rem call ./../plot_rdf.bat
+rem plot_rdf_all_v2.gpl
 cd ../
 
 echo "-------------------------------------------------------------"
 echo erate1e-4 %date% %time%
 cd ./erate1e-4
 call ../run_msmpi_v2.bat
+plot_stress_vs_strain_v2.gpl
+rem call ./../plot_rdf.bat
+rem plot_rdf_all_v2.gpl
 cd ../
 
 echo "-------------------------------------------------------------"
 echo erate1e-5 %date% %time%
 cd ./erate1e-5
 call ../run_msmpi_v2.bat
+plot_stress_vs_strain_v2.gpl
+rem call ./../plot_rdf.bat
+rem plot_rdf_all_v2.gpl
 cd ../
