2b22fd88b7906e2f094dc8196d7b7df9c16a9663
by-student-2017 <studentsctest@gmail.com>
2023-09-04 08:07:21 +0900

    windows11

--- references/Cu-Zr_based_metallic_glasses/4.2.1.4_Tensile_deformation_Ni-Zr-Nb-Al_glassy_alloys/sequential_test/run_seq.bat
+++ references/Cu-Zr_based_metallic_glasses/4.2.1.4_Tensile_deformation_Ni-Zr-Nb-Al_glassy_alloys/sequential_test/run_seq.bat
@@ -1,7 +1,7 @@
 echo "-------------------------------------------------------------"
 echo erate1e-1 %date% %time%
 cd ./erate1e-1
-call ../run_msmpi_v2.bat
+rem call ../run_msmpi_v2.bat
 plot_stress_vs_strain_v2.gpl
 rem call ./../plot_rdf.bat
 rem plot_rdf_all_v2.gpl
@@ -37,62 +37,6 @@ cd ../
 echo "-------------------------------------------------------------"
 echo erate1e-5 %date% %time%
 cd ./erate1e-5
-call ../run_msmpi_v2.bat
-plot_stress_vs_strain_v2.gpl
-rem call ./../plot_rdf.bat
-rem plot_rdf_all_v2.gpl
-cd ../
-
-
-echo "-------------------------------------------------------------"
-echo erate1e-6 %date% %time%
-cd ./erate1e-6
-call ../run_msmpi_v2.bat
-plot_stress_vs_strain_v2.gpl
-rem call ./../plot_rdf.bat
-rem plot_rdf_all_v2.gpl
-cd ../
-
-echo "-------------------------------------------------------------"
-echo erate1e-7 %date% %time%
-cd ./erate1e-7
-call ../run_msmpi_v2.bat
-plot_stress_vs_strain_v2.gpl
-rem call ./../plot_rdf.bat
-rem plot_rdf_all_v2.gpl
-cd ../
-
-echo "-------------------------------------------------------------"
-echo erate1e-8 %date% %time%
-cd ./erate1e-8
-call ../run_msmpi_v2.bat
-plot_stress_vs_strain_v2.gpl
-rem call ./../plot_rdf.bat
-rem plot_rdf_all_v2.gpl
-cd ../
-
-echo "-------------------------------------------------------------"
-echo erate1e-9 %date% %time%
-cd ./erate1e-9
-call ../run_msmpi_v2.bat
-plot_stress_vs_strain_v2.gpl
-rem call ./../plot_rdf.bat
-rem plot_rdf_all_v2.gpl
-cd ../
-
-echo "-------------------------------------------------------------"
-echo erate1e-10 %date% %time%
-cd ./erate1e-10
-call ../run_msmpi_v2.bat
-plot_stress_vs_strain_v2.gpl
-rem call ./../plot_rdf.bat
-rem plot_rdf_all_v2.gpl
-cd ../
-
-
-echo "-------------------------------------------------------------"
-echo erate1e-11 %date% %time%
-cd ./erate1e-11
 call ../run_msmpi_v2.bat
 plot_stress_vs_strain_v2.gpl
 rem call ./../plot_rdf.bat
