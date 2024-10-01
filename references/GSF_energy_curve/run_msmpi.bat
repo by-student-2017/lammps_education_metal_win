a562ce71aa0b7034af18cb9a6935c416c357ba62
by-student-2017 <33742249+by-student-2017@users.noreply.github.com>
2024-10-01 17:44:33 +0900

    Add files via upload

---
+++ references/GSF_energy_curve/run_msmpi.bat
@@ -0,0 +1,15 @@
+set ncore=4
+set OMP_NUM_THREADS=1
+
+del data.gsf
+
+mkdir cfg
+
+for %%1 in (in.gsf) do (
+  "C:\Program Files\Microsoft MPI\Bin\mpiexec.exe" -np %ncore% "C:\Program Files\LAMMPS 64-bit 22Dec2022-MSMPI\bin\lmp.exe" -in %%1
+  echo input file: %%1
+)
+
+plot.gpl
+
+pause
