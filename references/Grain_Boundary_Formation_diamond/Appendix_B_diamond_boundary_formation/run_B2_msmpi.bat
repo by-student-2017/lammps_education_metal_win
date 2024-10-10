5fccba2f518db1307ffce672137c67540f8a868a
by-student-2017 <33742249+by-student-2017@users.noreply.github.com>
2024-10-10 11:44:06 +0900

    Add files via upload

--- references/Grain_Boundary_Formation_diamond/Appendix_B_diamond_boundary_formation/run_B2_msmpi.bat
+++ references/Grain_Boundary_Formation_diamond/Appendix_B_diamond_boundary_formation/run_B2_msmpi.bat
@@ -1,7 +1,7 @@
 set ncore=8
 set OMP_NUM_THREADS=1
 
-rem mkdir cfg
+mkdir dump
 
 for %%1 in (in_B2.lmp) do (
   "C:\Program Files\Microsoft MPI\Bin\mpiexec.exe" -np %ncore% "C:\Program Files\LAMMPS 64-bit 22Dec2022-MSMPI\bin\lmp.exe" -in %%1
