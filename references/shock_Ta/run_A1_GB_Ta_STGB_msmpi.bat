7d9d36994afcdd06a06319a40314d5d8aa5a67ee
by-student-2017 <33742249+by-student-2017@users.noreply.github.com>
2024-10-15 16:23:52 +0900

    Add files via upload

---
+++ references/shock_Ta/run_A1_GB_Ta_STGB_msmpi.bat
@@ -0,0 +1,12 @@
+set ncore=8
+set OMP_NUM_THREADS=1
+
+rem mkdir cfg
+mkdir dump
+
+for %%1 in (in.GB_Ta_STGB) do (
+  "C:\Program Files\Microsoft MPI\Bin\mpiexec.exe" -np %ncore% "C:\Program Files\LAMMPS 64-bit 22Dec2022-MSMPI\bin\lmp.exe" -in %%1
+  echo input file: %%1
+)
+
+pause
