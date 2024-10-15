48ca5018bb76c1daac2c84f1b89a9e3d3c6ac691
by-student-2017 <33742249+by-student-2017@users.noreply.github.com>
2024-10-15 14:32:03 +0900

    Add files via upload

---
+++ references/Shock_Hugoniots_Al/run_shock_msmpi.bat
@@ -0,0 +1,11 @@
+set ncore=8
+set OMP_NUM_THREADS=1
+
+mkdir cfg
+
+for %%1 in (in_shock.lmp) do (
+  "C:\Program Files\Microsoft MPI\Bin\mpiexec.exe" -np %ncore% "C:\Program Files\LAMMPS 64-bit 22Dec2022-MSMPI\bin\lmp.exe" -in %%1
+  echo input file: %%1
+)
+
+pause
