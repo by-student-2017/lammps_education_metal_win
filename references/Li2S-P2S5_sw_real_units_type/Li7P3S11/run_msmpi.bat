71079664ab11deb27e0474d21ea4ca2c5c959ac1
by-student-2017 <33742249+by-student-2017@users.noreply.github.com>
2024-10-15 10:45:59 +0900

    Add files via upload

---
+++ references/Li2S-P2S5_sw_real_units_type/Li7P3S11/run_msmpi.bat
@@ -0,0 +1,11 @@
+set ncore=8
+set OMP_NUM_THREADS=1
+
+rem mkdir cfg
+
+for %%1 in (Li7P3S11.in) do (
+  "C:\Program Files\Microsoft MPI\Bin\mpiexec.exe" -np %ncore% "C:\Program Files\LAMMPS 64-bit 22Dec2022-MSMPI\bin\lmp.exe" -in %%1
+  echo input file: %%1
+)
+
+pause
