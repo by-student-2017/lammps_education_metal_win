b57c39b386170cee68cb633e9a06a9e6923da0fa
by-student-2017 <33742249+by-student-2017@users.noreply.github.com>
2024-10-16 09:58:51 +0900

    Add files via upload

---
+++ references/Bombardment_Li_ion/run_D_LiIonGun_msmpi.bat
@@ -0,0 +1,11 @@
+set ncore=4
+set OMP_NUM_THREADS=1
+
+mkdir cfg
+
+for %%1 in (LiIonGun.txt) do (
+  "C:\Program Files\Microsoft MPI\Bin\mpiexec.exe" -np %ncore% "C:\Program Files\LAMMPS 64-bit 22Dec2022-MSMPI\bin\lmp.exe" -in %%1
+  echo input file: %%1
+)
+
+pause
