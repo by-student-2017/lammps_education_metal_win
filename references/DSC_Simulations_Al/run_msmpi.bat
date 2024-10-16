a6b2fc3916b4e94d8ca7e62e421a876cba9a7f11
by-student-2017 <33742249+by-student-2017@users.noreply.github.com>
2024-10-16 19:02:04 +0900

    Add files via upload

--- references/DSC_Simulations_Al/run_msmpi.bat
+++ references/DSC_Simulations_Al/run_msmpi.bat
@@ -1,4 +1,4 @@
-set ncore=4
+set ncore=8
 set OMP_NUM_THREADS=1
 
 mkdir cfg
@@ -8,5 +8,7 @@ for %%1 in (input.heating_loop_3keVAl) do (
   "C:\Program Files\Microsoft MPI\Bin\mpiexec.exe" -np %ncore% "C:\Program Files\LAMMPS 64-bit 22Dec2022-MSMPI\bin\lmp.exe" -in %%1
   echo input file: %%1
 )
+
+plot.gpl
 
 pause
