87beb87586dcd4615ff59d6600188ad7a4c3f647
by-student-2017 <33742249+by-student-2017@users.noreply.github.com>
2024-10-21 17:17:29 +0900

    Add files via upload

---
+++ references/cohesive_energy_metal_LJ/run_msmpi.bat
@@ -0,0 +1,12 @@
+set ncore=8
+set OMP_NUM_THREADS=1
+
+rem mkdir cfg
+rem mkdir dump
+
+for %%1 in (in.lmp) do (
+  "C:\Program Files\Microsoft MPI\Bin\mpiexec.exe" -np %ncore% "C:\Program Files\LAMMPS 64-bit 22Dec2022-MSMPI\bin\lmp.exe" -in %%1
+  echo input file: %%1
+)
+
+pause
