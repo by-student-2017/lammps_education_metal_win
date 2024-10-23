822b563129dd290da29648af296f5932fda4eab6
by-student-2017 <33742249+by-student-2017@users.noreply.github.com>
2024-10-23 13:48:36 +0900

    Add files via upload

---
+++ references/Shape_Entropy/6-hex-periodic/run_msmpi.bat
@@ -0,0 +1,10 @@
+set ncore=4
+
+rem mkdir cfg
+
+for %%1 in (6-hex-periodic-boundary-script.txt) do (
+  "C:\Program Files\Microsoft MPI\Bin\mpiexec.exe" -np %ncore% "C:\Program Files\LAMMPS 64-bit 22Dec2022-MSMPI\bin\lmp.exe" -in %%1
+  echo input file: %%1
+)
+
+pause
