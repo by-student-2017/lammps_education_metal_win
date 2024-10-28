268c4a55aaccde74d3c2e44d7cf9587057ef90cd
by-student-2017 <33742249+by-student-2017@users.noreply.github.com>
2024-10-28 17:37:30 +0900

    Add files via upload

---
+++ references/WSe2/run_msmpi.bat
@@ -0,0 +1,14 @@
+set ncore=4
+set OMP_NUM_THREADS=1
+
+rem mkdir cfg
+mkdir dump
+
+for %%1 in (in.tension) do (
+  "C:\Program Files\Microsoft MPI\Bin\mpiexec.exe" -np %ncore% "C:\Program Files\LAMMPS 64-bit 22Dec2022-MSMPI\bin\lmp.exe" -in %%1
+  echo input file: %%1
+)
+
+plot_stress_vs_strain_v2.gpl
+
+pause
