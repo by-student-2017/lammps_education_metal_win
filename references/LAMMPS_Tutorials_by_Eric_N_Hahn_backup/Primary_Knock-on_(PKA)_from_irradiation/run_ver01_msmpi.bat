1f90ed1e941ecc7def8b63e421323bcc566953f0
by-student-2017 <33742249+by-student-2017@users.noreply.github.com>
2024-10-29 16:00:24 +0900

    Add files via upload

---
+++ references/LAMMPS_Tutorials_by_Eric_N_Hahn_backup/Primary_Knock-on_(PKA)_from_irradiation/run_ver01_msmpi.bat
@@ -0,0 +1,12 @@
+set ncore=4
+set OMP_NUM_THREADS=1
+
+mkdir cfg
+mkdir dump
+
+for %%1 in (in_ver01.lmp) do (
+  "C:\Program Files\Microsoft MPI\Bin\mpiexec.exe" -np %ncore% "C:\Program Files\LAMMPS 64-bit 22Dec2022-MSMPI\bin\lmp.exe" -in %%1
+  echo input file: %%1
+)
+
+pause
