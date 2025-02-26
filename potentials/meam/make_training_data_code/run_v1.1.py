0a57f18edfb993e6787e80fc1a96dddfed6b307c
By STUDENT <33742249+by-student-2017@users.noreply.github.com>
2025-02-26 15:55:10 +0900

    Add files via upload

--- potentials/meam/make_training_data_code/run_v1.1.py
+++ potentials/meam/make_training_data_code/run_v1.1.py
@@ -264,7 +264,7 @@ def fit_rose_curve_erose_form_1(volumes_per_atom, cohesive_energies_per_atom, al
     plt.plot(volumes_per_atom, rose_curve(volumes_per_atom, alpha, V0, Ec, repuls_fit, attrac_fit, nearest_neighbor_distance), 
              label=f'Rose Curve Fit (repuls={repuls_fit:.4f}, attrac={attrac_fit:.4f})', color='red')
     plt.plot(volumes_per_atom, rose_curve(volumes_per_atom, alpha, V0, Ec, 0, 0, nearest_neighbor_distance),  
-         label=f'Rose Curve Fit with (repuls=0, attrac=0)', color='blue')
+             label=f'Rose Curve Fit (repuls=0, attrac=0)', color='blue')
     plt.plot(volumes_per_atom, rose_curve(volumes_per_atom, alpha, V0, Ec, 0.05, 0.05, nearest_neighbor_distance),  
              label=f'Rose Curve Fit (repuls=0.05, attrac=0.05)', color='green')
     plt.xlabel('Volume, V (A^3/atom)')
