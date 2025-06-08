c1d0daccf9213792ebed51becb977d2d5eb07137
By STUDENT <33742249+by-student-2017@users.noreply.github.com>
2025-06-08 16:09:27 +0900

    Update json2library_template.py

--- potentials/meam/meam_fit/json2library_template.py
+++ potentials/meam/meam_fit/json2library_template.py
@@ -64,7 +64,7 @@ if element in data:
     template = """# 2NN-MEAM
 rc = 5.8          # default = 4.0 (4.7 <= K >= 5.9, Rb >= 5.1, Cs > 5.3, CHGNET:6.0)
 delr = 0.1        # default = 0.1
-augt1 = 1         # default = 1 (1NN-MEAM:0, 1NN->2NN:0, 2NN-MEAM:1) (1NN: 0 or 1 gives the same result)
+augt1 = 1         # default = 1 (1NN-MEAM:0, 1NN->2NN:0, 2NN-MEAM:1) (1NN-MEAM: 0 or 1 gives the same result)
 ialloy = 0        # default = 0 (same reference structure:0, 2NN-MEAM:2)
 emb_lin_neg = 0   # default = 0
 bkgd_dyn = 0      # default = 0
