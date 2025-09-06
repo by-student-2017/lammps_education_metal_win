2f95614864dd3e3a909f8b7e855878fde40b2755
By STUDENT <33742249+by-student-2017@users.noreply.github.com>
2025-09-06 20:55:28 +0900

    Add files via upload

--- potentials/meam/make_training_data_code/run_alloy_elastic_v1.1.py
+++ potentials/meam/make_training_data_code/run_alloy_elastic_v1.1.py
@@ -64,11 +64,10 @@ npoints = 1 # >= 7 e.g., 7, 11, 17, 21, or 25, etc (Recommend >= 25), (default =
 #fixed_element = 'S'
 fixed_element = 'YYYYYYYYYY'
 elements = [fixed_element,
-            # 'H',                                                                                                 'He',
-            #'Li', 'Be',                                                              'B',  'C',  'N',  'O',  'F', 'Ne',
-            #'Na', 'Mg',                                                             'Al', 'Si',  'P',  'S', 'Cl', 'Ar',
-            # 'K', 'Ca', 'Sc', 'Ti',  'V', 
-            'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn', 'Ga', 'Ge', 'As', 'Se', 'Br', 'Kr',
+             'H',                                                                                                 'He',
+            'Li', 'Be',                                                              'B',  'C',  'N',  'O',  'F', 'Ne',
+            'Na', 'Mg',                                                             'Al', 'Si',  'P',  'S', 'Cl', 'Ar',
+             'K', 'Ca', 'Sc', 'Ti',  'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn', 'Ga', 'Ge', 'As', 'Se', 'Br', 'Kr',
             'Rb', 'Sr',  'Y', 'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd', 'In', 'Sn', 'Sb', 'Te',  'I', 'Xe',
             'Cs', 'Ba', 'La', 
                         'Ce', 'Pr', 'Nd', 'Pm', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm', 'Yb', 'Lu', 
